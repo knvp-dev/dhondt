e106616a1804339e3d0371059ab9b8556d029b17
webdesigner-konvert <webdesigner@konvert.be>
2017-12-08 14:23:06 +0100

    initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+	<div class="container">
+		<div class="footer-content">
+			<div class="has-text-centered">
+				<img src="/images/logo.svg" alt="" width="200">
+				<br><br>
+				<p>
+				Nikolaj d'hondt <br>
+				 <br>
+				 <br>
+				 <br>
+				</p>
+			</div>
+		</div>
+	</div>
+	
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar has-shadow">
+	<div class="container">
+		<div class="navbar-start">
+			<a class="navbar-item" href="/">
+				<img src="images/logo.svg" width="100%" height="150" alt="Dhondt">
+			</a>
+		</div>
+
+		<div class="navbar-end">
+			<a class="navbar-item" href="#">
+				Bouwwerken
+			</a>
+		</div>
+
+	</div>
+</nav>
